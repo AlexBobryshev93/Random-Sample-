21174c12b36d1661e17fda34bc5d58bae04f9896
AlexBobryshev <darknessrider1@rambler.ru>
2019-05-20 15:18:30 +0300

    init commit

---
+++ Test1.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+class Test1 {
+	public static void main(String[] args) {
+		Random r = new Random();
+		System.out.println(r.nextInt()); // from 0 to 9
+		1;
+		(r.nextInt());
+	}
+}
