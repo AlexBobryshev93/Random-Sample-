5d666f87dd3e69c44cfe5dde082f74aadede39cf
AlexBobryshev <darknessrider1@rambler.ru>
2019-06-04 23:22:03 +0300

    3rd commit

--- Test1.java
+++ Test1.java
@@ -4,6 +4,6 @@ class Test1 {
 	public static void main(String[] args) {
 		Random r = new Random();
 		System.out.println(r.nextInt(10)); // from 0 to 9
-		System.out.println(r.nextDouble(10)); // from 0 to 9
+		System.out.println(r.nextDouble()); // from 0 to 1
 	}
 }
