605091de8063d8331a3969980de41387e3d8746d
AlexBobryshev <darknessrider1@rambler.ru>
2019-06-04 23:18:41 +0300

    1st commit

--- Test1.java
+++ Test1.java
@@ -3,8 +3,6 @@ import java.util.Random;
 class Test1 {
 	public static void main(String[] args) {
 		Random r = new Random();
-		System.out.println(r.nextInt()); // from 0 to 9
-		1;
-		(r.nextInt());
+		System.out.println(r.nextInt(9)); // from 0 to 9
 	}
 }
